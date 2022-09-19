a39d92d04ad479127786110adc54d1c4f2c984a3
gayohignasius <gayohignasius>
2022-09-19 21:23:22 +0700

    add test case using jest

---
+++ src/user/register.test.js
@@ -0,0 +1,15 @@
+const userService = require("./user.service");
+
+describe("test user sign up", () => {
+  it("can sign up as new user", async () => {
+    const res = await request(app)
+      .post(`${process.env.URL}/api/registration`)
+      .send({
+        fullName: "Ignasius Gayoh",
+        email: "gayohignasius@gmail.com",
+        password: "Password@123!?#",
+      });
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("user");
+  });
+});
